6557350bf666ac00094b11e73af701721a553fbd
oualidrahhouti <rahhoutiwalid@gmail.com>
2025-09-25 21:45:48 +0200

    adding title and menu buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimaryButtonComponent } from './shared/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './shared/secondary-button/secondary-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimaryButtonComponent, SecondaryButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title = 'Tour of heroes';
 }
