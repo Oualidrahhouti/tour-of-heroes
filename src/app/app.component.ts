f0f97416ec1749ce081ca24cbbd3d4b3f57545ea
oualidrahhouti <rahhoutiwalid@gmail.com>
2025-10-07 18:05:30 +0200

    adding routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterModule } from '@angular/router';
 import { SecondaryButtonComponent } from './shared/secondary-button/secondary-button.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecondaryButtonComponent, DashboardComponent],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    SecondaryButtonComponent,
+    DashboardComponent,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { HeroesComponent } from './features/heroes/components/heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'heroes',
+    component: HeroesComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
