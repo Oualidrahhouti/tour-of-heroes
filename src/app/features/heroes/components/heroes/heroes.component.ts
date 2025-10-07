c6e51ba7049077e3ed4256e87ebe1fe84d5e06ce
oualidrahhouti <rahhoutiwalid@gmail.com>
2025-10-07 18:05:13 +0200

    init heroes component

---
+++ src/app/features/heroes/components/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      heroes works!
+    </p>
+  `,
+  styles: ``
+})
+export class HeroesComponent {
+
+}
