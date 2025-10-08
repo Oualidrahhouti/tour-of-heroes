741dfd99f1e18e035ef4035c4a9fc14b9a87d715
oualidrahhouti <rahhoutiwalid@gmail.com>
2025-10-08 19:02:13 +0200

    implementing in-memory-data

--- src/app/core/services/inMemoryData/in-memory-data.service.ts
+++ src/app/core/services/inMemoryData/in-memory-data.service.ts
@@ -6,21 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
-  heroes = [
-    { id: 12, name: 'Dr. Nice' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr. IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' },
-    { id: 21, name: 'Oualid' },
-  ];
   constructor() {}
 
   createDb(): {} | Observable<{}> | Promise<{}> {
-    return this.heroes;
+    const heroes = [
+      { id: 12, name: 'Dr. Nice' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr. IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' },
+      { id: 21, name: 'Oualid' },
+    ];
+    return { heroes };
   }
 }
