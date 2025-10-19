15decde4a525a64352e44b271a71dfa47caf3956
oualidrahhouti <rahhoutiwalid@gmail.com>
2025-10-19 16:27:50 +0200

    moving component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { HeroesComponent } from './features/heroes/components/heroes/heroes.component';
-import { HeroeComponent } from './features/heroes/heroe/heroe.component';
+import { HeroeComponent } from './features/heroes/components/heroe/heroe.component';
 
 export const routes: Routes = [
   {
